0b545b43675465c904199e4eeabe69301ffc9c6d
xcaptdz <xcaptdz@gmail.com>
2025-05-02 18:24:23 +0300

    added api v1

---
+++ src/app/api/v1/webhooks/data.ts
@@ -0,0 +1,21 @@
+export const comments = [
+
+    {
+        id: 1,
+        text: "Very good job!"
+    
+    },
+    {
+        id: 2,
+        text: "Good start :)  "
+    
+    },
+    {
+        id: 3,
+        text: "keep trying!"
+    
+    }
+    
+    
+    
+    ]
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1>Webhook API is running!</h1>;
+}
