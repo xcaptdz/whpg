a22b5b9851533fcf32e0f6e05f3ccd99d1606ece
xcaptdz <xcaptdz@gmail.com>
2025-05-03 07:52:08 +0300

    upadated post request for db insertion

--- src/app/api/v1/webhooks/route.ts
+++ src/app/api/v1/webhooks/route.ts
@@ -1,18 +1,13 @@
 
 
 
-import {comments} from "./data";
+
 
 import { NextResponse } from 'next/server';
 import { prisma } from '../../../../../lib/prisma';
 
 
 
-
-// export async function GET() {
-
-//     return   Response.json(comments);
-// }
 
 export async function GET() {
     try {
@@ -29,18 +24,67 @@ export async function GET() {
 
 
 
-export async function POST(request : Request) {
+  export async function POST(request: Request) {
+    try {
+      const body = await request.json();
+      const { hook } = body;
+  
+      const newHook = await prisma.hook.create({
+        data: {
+          hook
+           
+        },
+      });
+  
+      return NextResponse.json(newHook, { status: 201 });
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return NextResponse.json(
+        { error: 'Failed to create user' },
+        { status: 500 }
+      );
+    }
+  }
 
-    const comment = await request.json();
-    const newComment = {
-        id : comments.length + 1,
-        text : comment.text,  
 
-    };
-    comments.push(newComment);
 
-     return new Response(JSON.stringify(newComment), {
-        headers: {"Content-Type":"application/json"},
-        status:201,
-     });
-}
+
+
+
+
+
+
+
+
+// import {comments} from "./data";
+
+
+
+// export async function GET() {
+
+//     return   Response.json(comments);
+// }
+
+
+
+
+
+
+
+
+
+  // export async function POST(request : Request) {
+
+//     const comment = await request.json();
+//     const newComment = {
+//         id : comments.length + 1,
+//         text : comment.text,  
+
+//     };
+//     comments.push(newComment);
+
+//      return new Response(JSON.stringify(newComment), {
+//         headers: {"Content-Type":"application/json"},
+//         status:201,
+//      });
+// }
