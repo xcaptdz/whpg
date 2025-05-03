d0d3251e2f0e3e24c3749f77c0a62329bd5d99ae
xcaptdz <xcaptdz@gmail.com>
2025-05-03 07:18:19 +0300

    implemented GET from db

--- src/app/api/v1/webhooks/page.tsx
+++
@@ -1,7 +0,0 @@
-import { prisma } from "../../../../../lib/prisma";
-
-
-export default async function handler(req, res) {
-    const hooks = await prisma.hook.findMany();
-    res.json(hooks);
-  }
